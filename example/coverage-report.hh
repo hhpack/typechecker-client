f3017f09449f0eb98209e09e60cb9ad8da188cce
holyshared <holy.shared.design@gmail.com>
2016-01-14 09:54:35 +0900

    update example

--- example/coverage-report.hh
+++ example/coverage-report.hh
@@ -16,21 +16,24 @@ async function coverage_select_main(string $cwd) : Awaitable<void>
     await $client->restart();
 
     $result = await $client->coverage();
-    $files = $result->filter(($item) ==> {
-        return $item instanceof File;
-    });
+    $files = $result->filter(($file) ==> $file instanceof File)
+        ->filter(($file) ==> preg_match('/typechecker-client\/src/', $file->name()) === 1);
+
+    echo 'Files:', PHP_EOL;
 
     foreach ($files as $file) {
-        $formattedParsentage = sprintf('%6.2f%%', (float) $file->parsentage() * 100); 
+        $formattedParsentage = sprintf('%6.2f%%', (float) $file->parsentage() * 100);
         echo $formattedParsentage, ' ', $file->name(), PHP_EOL;
     }
+    echo PHP_EOL;
 
-    $directories = $result->filter(($item) ==> {
-        return $item instanceof Directory;
-    });
+    $directories = $result->filter(($directory) ==> $directory instanceof Directory)
+        ->filter(($directory) ==> preg_match('/typechecker-client\/src/', $directory->name()) === 1);
+
+    echo 'Directories:', PHP_EOL;
 
     foreach ($directories as $directory) {
-        $formattedParsentage = sprintf('%6.2f%%', (float) $directory->parsentage() * 100); 
+        $formattedParsentage = sprintf('%6.2f%%', (float) $directory->parsentage() * 100);
         echo $formattedParsentage, ' ', $directory->name(), PHP_EOL;
     }
 }
